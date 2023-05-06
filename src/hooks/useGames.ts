66a3b1fd74c202990f9704ac4ce8ac16a9813d91
Anas Alhealy <56006995+AnasAlhealy@users.noreply.github.com>
2023-05-06 19:30:20 +0200

    Create a generic data fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClint from "../services/api-clint";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform {
   platform: any;
@@ -17,35 +15,6 @@ export interface Game {
   metacritic: number;
 }
 
-interface fetchGameResponse {
-  count: number;
-  results: Game[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClint
-      .get<fetchGameResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
